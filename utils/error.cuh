4629c435aefb1840df9dc1048926da6b9393003d
Youki Tohno <33323998+TohnoYouki@users.noreply.github.com>
2022-03-28 13:03:18 +0800

    Add files via upload

--- utils/error.cuh
+++ utils/error.cuh
@@ -4,24 +4,23 @@
 #include "cuda_runtime.h"
 
 #define CUDA_CHECK(status)                                       \
-	do {                                                         \
-		cudaError_t status_ = status;                            \
-		if (cudaSuccess != status_) {                            \
-			auto msg = cudaGetErrorString(status_);              \
-			std::printf("failed with status %d: %s at %s:%d\n",  \
-                         status_, msg, __FILE__, __LINE__);      \
-			throw std::logic_error("cuda API failed!");          \
-		}                                                        \
-	} while (0)
-
+    do {                                                         \
+        cudaError_t status = status;                             \
+        if (cudaSuccess != status_) {                            \
+            auto msg = cudaGetErrorString(status_);              \
+            std::printf("failed with status %d: %s at %s:%d\n",  \
+                        status_, msg, __FILE__, __LINE__);       \
+            throw std::logic_error("cuda API failed!");          \
+        }                                                        \
+    } while (0)
 
 #define CUDA_CHECK_ERROR(msg)                                    \
-	do {                                                         \
-		cudaError_t status = cudaGetLastError();                 \
-		if (cudaSuccess != status) {                             \
-			auto error_msg = cudaGetErrorString(status);         \
-			std::printf("%s: %s in %s at line %d\n",             \
+    do {                                                         \
+        cudaError_t status = cudaGetLastError();                 \
+        if (cudaSuccess != status) {                             \
+            auto error_msg = cudaGetErrorString(status);         \
+            std::printf("%s: %s in %s at line %d\n",             \
                         msg, error_msg, __FILE__, __LINE__);     \
-			throw std::logic_error("cuda Logic Error!");         \
-		}                                                        \
-	} while (0)
+            throw std::logic_error("cuda Logic Error!");         \
+        }                                                        \
+    } while (0)
