b19035ff0dbfc719196d4517dd54dc478d01b70b
Youki Tohno <33323998+TohnoYouki@users.noreply.github.com>
2022-03-28 12:44:49 +0800

    Add files via upload

---
+++ utils/test.cuh
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include "cuda_runtime.h"
+
+#define CUDA_CHECK(status)           \
+    do {                             \
+                                     \
+    } while (0)                     
