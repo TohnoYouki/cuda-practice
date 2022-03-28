2f905cd62bd25127c1085aad8a48e36bfcdd681b
Youki Tohno <33323998+TohnoYouki@users.noreply.github.com>
2022-03-28 13:11:25 +0800

    Add files via upload

--- utils/timer.cuh
+++ utils/timer.cuh
@@ -5,36 +5,36 @@
 
 class CpuTimer {
 private:
-	clock_t start, stop;
+    clock_t start, stop;
 public:
-	CpuTimer() {
-		start = clock();
-	}
-	~CpuTimer() {
-		stop = clock();
-		double time = 0.0;
-		time = double(stop - start) * 1000 / CLOCKS_PER_SEC;
-		std::cout << "Time: " << time << " ms" << std::endl;
-	}
+    CpuTimer() {
+        start = clock();
+    }
+    ~CpuTimer() {
+        stop = clock();
+        double time = 0.0;
+        time = double(stop - start) * 1000 / CLOCKS_PER_SEC;
+        std::cout << "Time: " << time << " ms" << std::endl;
+    }
 };
 
 class CudaTimer {
 private:
-	cudaEvent_t start, stop;
+    cudaEvent_t start, stop;
 public:
-	CudaTimer() { 
-		cudaEventCreate(&start);
-		cudaEventCreate(&stop);
-		cudaEventRecord(start, 0);
-	}
-	~CudaTimer() {
-		cudaEventRecord(stop, 0);
-		cudaEventSynchronize(stop);
-		while (cudaEventQuery(stop) == cudaErrorNotReady);
-		float time;
-		cudaEventElapsedTime(&time, start, stop);
-		std::cout << "Time: " << time << " ms" << std::endl;
-		cudaEventDestroy(start);
-		cudaEventDestroy(stop);
-	}
+    CudaTimer() { 
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        cudaEventRecord(start, 0);
+    }
+    ~CudaTimer() {
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        while (cudaEventQuery(stop) == cudaErrorNotReady);
+        float time;
+        cudaEventElapsedTime(&time, start, stop);
+        std::cout << "Time: " << time << " ms" << std::endl;
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
 };
